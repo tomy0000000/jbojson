927dccc17baee0c9c2b5055665cd8ca20769e735
Tomy Hsieh <git@tomy.me>
2026-05-11 01:18:52 -0700

    🗄 [chore] Update ESlint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,32 @@
-const { default: json } = require("@eslint/json")
+import { defineConfig } from "eslint/config"
+import jsonc from "eslint-plugin-jsonc"
 
-module.exports = [
+export default defineConfig([
+  { ignores: ["test/fixtures/**"] },
+  ...jsonc.configs["flat/recommended-with-json"],
   {
-    plugins: { json },
-    files: ["**/*.json"],
-    language: "json/json5",
+    files: ["data/**/*.json", "schema/**/*.json", "test/fixtures/*.json"],
     rules: {
-      "json/no-duplicate-keys": "error",
-      "json/no-empty-keys": "error",
-      "json/sort-keys": "error",
+      "jsonc/array-bracket-newline": [
+        "error",
+        { multiline: true, minItems: 1 },
+      ],
+      "jsonc/array-bracket-spacing": ["error", "never"],
+      "jsonc/array-element-newline": ["error", "always"],
+      "jsonc/indent": ["error", 2],
+      "jsonc/key-name-casing": "error",
+      "jsonc/key-spacing": "error",
+      "jsonc/object-curly-newline": ["error", "always"],
+      "jsonc/object-property-newline": "error",
+      "jsonc/sort-array-values": [
+        "error",
+        {
+          pathPattern: ".*",
+          order: { type: "asc" },
+        },
+      ],
+      "jsonc/sort-keys": "error",
+      "no-trailing-spaces": "error",
     },
   },
-]
+])
